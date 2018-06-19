cd1db4007ba825e2488bbecfaf54aa819d220c57
yangtingting <15210156211@163.com>
2018-06-19 16:41:50 +0800

    pizzaStore

---
+++ src/main/java/com/example/headfirst/restaurant/Iterator.java
@@ -0,0 +1,6 @@
+package com.example.headfirst.restaurant;
+
+public interface Iterator {
+    boolean hasNext();
+    Object next();
+}
---
+++ src/main/java/com/example/headfirst/restaurant/Waitress.java
@@ -0,0 +1,29 @@
+package com.example.headfirst.restaurant;
+
+public class Waitress {
+    DinerMenu dinerMenu;
+    PancakeHouseMenu pancakeHouseMenu;
+
+    public Waitress(DinerMenu dinerMenu, PancakeHouseMenu pancakeHouseMenu) {
+        this.dinerMenu = dinerMenu;
+        this.pancakeHouseMenu = pancakeHouseMenu;
+    }
+
+    public void printMenu(){
+        Iterator pancakeIterator = dinerMenu.createIterator();
+        Iterator dinerIterator = pancakeHouseMenu.createIterator();
+        System.out.println("MENU\n----\nBREAKFAST");
+        this.printMenu(pancakeIterator);
+        System.out.println("\nLUNCH");
+        this.printMenu(dinerIterator);
+    }
+    private void printMenu(Iterator iterator){
+        while (iterator.hasNext()){
+            MenuItem menuItem = (MenuItem) iterator.next();
+            System.out.print(menuItem.getName()+",");
+            System.out.print(menuItem.getPrice());
+            System.out.println(menuItem.getDescription());
+            System.out.println(menuItem.isVegetarian()?"素食":"");
+        }
+    }
+}
